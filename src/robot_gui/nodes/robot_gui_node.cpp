53662ff13e0ee1e38b528df2b1e18688e65e493c
nguyenlamanhvu <anv2003bmt@gmail.com>
2025-01-05 04:25:23 +0700

    [develop]: add update OTA node

--- src/robot_gui/nodes/robot_gui_node.cpp
+++ src/robot_gui/nodes/robot_gui_node.cpp
@@ -1,6 +1,6 @@
 #include <QApplication>
 #include "robot_gui.h"
-
+#include <QIcon>
 int main(int argc, char *argv[])
 {
   ros::init(argc, argv, "robot_gui_node", ros::init_options::AnonymousName);
@@ -8,6 +8,8 @@ int main(int argc, char *argv[])
 
   robot_gui w;
   w.setWindowTitle(QString::fromStdString(ros::this_node::getName()));
+  QIcon icon(":/Icons/Search_Icon.png");
+  w.setWindowIcon(icon);
   w.show();
   return a.exec();
 }
--- src/robot_gui/src/robot_gui.cpp
+++ src/robot_gui/src/robot_gui.cpp
@@ -1,10 +1,13 @@
 #include "robot_gui.h"
 #include "ui_robot_gui.h"
+#include <QIcon>
 
 robot_gui::robot_gui(QWidget *parent) :
   QWidget(parent),
   ui(new Ui::robot_gui)
 {
+  QIcon icon(":/Icons/Search_Icon.png");
+//  ui->btSearch->setIcon(icon);
   ui->setupUi(this);
 }
 
